5e5c93371688af49b461bb4bb55192237637af32
Felipe Araújo Quirino <felipeaquirino@gmail.com>
2025-05-10 09:27:48 -0300

    Ajuste na entrada de dados do usuário e correção de caminho para acesso às pastas

--- reader/reader.py
+++ reader/reader.py
@@ -1,40 +1,53 @@
 import json
 import os
+import datetime
 
 class Texto:
     def __init__(self, titulo, autor=None, texto=None):
         self.titulo = titulo
         self.autor = autor
         self.texto = texto
-        self.caminho = os.path.join(*['C', 'Users', 'felip', 'OneDrive', 'Documentos'])
+        self.date = str(datetime.datetime.now())
+        self.caminho = os.path.join(*["C:\\", "Users", "felip", "OneDrive", "Documentos", titulo+".json"])
 
     def salvar(self) ->None:
         novo = {
             'titulo': self.titulo,
             'autor': self.autor,
-            'texto': self.texto
+            'texto': self.texto,
+            'data': self.date
         }
-        with open(os.path.join(self.caminho, self.titulo+'.json'), mode='w', encoding='utf-8') as s:
+        with open(self.caminho, mode='w', encoding='utf-8') as s:
             json.dump(novo, s, ensure_ascii=False, allow_nan=False, indent=4)
 
     def ler(self) ->None:
-        with open(os.path.join(self.caminho, self.titulo+'.json'), mode='r', encoding='utf-8') as l:
+        with open(self.caminho, mode='r', encoding='utf-8') as l:
             arq = json.load(l)
             print(f"Título: {arq['titulo'].capitalize()}")
             print(f"Autor: {arq['autor'].capitalize()}")
+            print(f"Criado em: {arq['data']}")
             print('')
             print(f"{arq['texto']}")
 
 def salvar_texto() ->None:
-    s_titulo = str(input('Título: '))
+    while True:
+        s_titulo = str(input('Título: '))
+        if s_titulo:
+            break
+        else:
+            print('ERRO! Texto precisa ter um título!')
     teste = Texto(s_titulo)
-    arquivo = os.path.join(teste.caminho, teste.titulo + '.json')
-    if os.path.exists(arquivo):
+    if os.path.exists(teste.caminho):
         alerta = str(input('Arquivo já existe! Deseja continuar [S/N]? '))
         if alerta.upper().strip() != 'S':
             print('Operação cancelada!')
             return
-    s_autor = str(input('Autor: '))
+    while True:
+        s_autor = str(input('Autor: '))
+        if s_autor:
+            break
+        else:
+            print('ERRO! Texto precisa ter autor!')
     linhas = []
     aux = 0
     while True:
@@ -51,7 +64,12 @@ def salvar_texto() ->None:
     salva.salvar()
 
 def ler_texto() ->None:
-    l_titulo = str(input('Título: '))
+    while True:
+        l_titulo = str(input('Título: '))
+        if l_titulo:
+            break
+        else:
+            print('ERRO! Digite um título!')
     le = Texto(l_titulo)
     try:
         le.ler()
