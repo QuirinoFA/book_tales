4f2583e4f64577b410e5b1fe101ff5bb8d8786f1
Felipe Araújo Quirino <felipeaquirino@gmail.com>
2025-06-18 22:57:32 -0300

    Atualização dos scripts do __init__ e do universo

--- reader/__init__.py
+++ reader/__init__.py
@@ -1,3 +1,5 @@
 from .texto import Texto, salvar_texto, ler_texto
-from .universo import (Universo, salvar_universo, ler_universo, atualizar_universo, deletar_universo,
-                       ArquivoCorrompido, ArquivoNaoEncontrado, UniversoNaoEncontrado, UniversoJaExiste)
+from .universo import Universo
+from .repositorio import RepositorioUniverso
+from .universo_utilidades import salvar_universo, ler_universo, atualizar_universo, deletar_universo
+from .erros import ArquivoCorrompido, ArquivoNaoEncontrado, UniversoNaoEncontrado, UniversoJaExiste
--- reader/universo.py
+++ reader/universo.py
@@ -1,6 +1,7 @@
-import json
 import os
 import datetime
+from repositorio import RepositorioUniverso
+from erros import UniversoJaExiste
 
 
 caminho = os.path.join(*["C:\\", "Users", "felip", "OneDrive", "Documentos", "Tabela_Relacional.json"])
@@ -11,7 +12,7 @@ class Universo:
         self.resumo = None
         self.criado = None
         self.alterado = None
-        self.repositorio = RepositorioUniverso()
+        self.repositorio = RepositorioUniverso(caminho)
 
     def para_dict(self) ->dict:
         dic_universo = {
@@ -56,134 +57,3 @@ class Universo:
         self.resumo = None
         self.criado = None
         self.alterado = None
-
-
-class ErroArquivo(Exception):
-    pass
-
-class ArquivoNaoEncontrado(ErroArquivo):
-    def __init__(self, a_n_e_caminho):
-        self.caminho = a_n_e_caminho
-        super().__init__(f"Arquivo não encontrado. Caminho: {self.caminho}")
-
-class ArquivoCorrompido(ErroArquivo):
-    def __init__(self, a_c_caminho):
-        self.caminho = a_c_caminho
-        super().__init__(f"Arquivo corrompido. Caminho: {self.caminho}")
-
-class ErroUniverso(Exception):
-    pass
-
-class UniversoJaExiste(ErroUniverso):
-    def __init__(self, u_j_e_caminho):
-        self.caminho = u_j_e_caminho
-        super().__init__(f"Universo já existe. Caminho {self.caminho}")
-
-class UniversoNaoEncontrado(ErroUniverso):
-    def __init__(self,  u_n_e_caminho):
-        self.caminho = u_n_e_caminho
-        super().__init__(f"Universo não encontrado. Caminho {self.caminho}")
-
-class RepositorioUniverso:
-    def __init__(self):
-        self.caminho = caminho
-
-    def ler_arquivo(self) -> list:
-        try:
-            with open(self.caminho, mode='r', encoding='utf-8') as j_l_a:
-                tab_l_a = json.load(j_l_a)
-            return tab_l_a
-        except FileNotFoundError:
-            raise ArquivoNaoEncontrado(self.caminho)
-        except json.JSONDecodeError:
-            raise ArquivoCorrompido(self.caminho)
-
-    def salvar_arquivo(self, tab_s_a:list) -> bool:
-        try:
-            with open(self.caminho, mode='w', encoding='utf-8') as j_s_a:
-                json.dump(tab_s_a, j_s_a, ensure_ascii=False, allow_nan=False, indent=4)
-            return True
-        except FileNotFoundError:
-            raise ArquivoNaoEncontrado(self.caminho)
-        except json.JSONDecodeError:
-            raise ArquivoCorrompido(self.caminho)
-
-    def existe_universo(self, e_u_nome:str) -> bool:
-        tab_e_u = self.ler_arquivo()
-        for e_universo in tab_e_u:
-            if e_universo.get('Nome') == e_u_nome:
-                return True
-        return False
-
-    def salvar_universo(self, s_u_novo:dict) ->bool:
-        tab_s_u = self.ler_arquivo()
-        tab_s_u.append(s_u_novo.copy())
-        self.salvar_arquivo(tab_s_u)
-        return True
-
-    def ler_universo(self, l_u_nome:str) ->dict:
-        tab_l_u = self.ler_arquivo()
-        for l_universo in tab_l_u:
-            if l_universo.get('Nome') == l_u_nome:
-                return l_universo
-        raise UniversoNaoEncontrado(self.caminho)
-
-    def atualizar_universo(self, v_u_nome:str, a_u_universo:dict) ->bool:
-        tab_a_u = self.ler_arquivo()
-        for a_universo in tab_a_u:
-            if a_universo['Nome'] == v_u_nome:
-                a_universo['Nome'] = a_u_universo['Nome']
-                a_universo['Resumo'] = a_u_universo['Resumo']
-                a_universo['Criado'] = a_u_universo['Criado']
-                a_universo['Alterado'] = a_u_universo['Alterado']
-                self.salvar_arquivo(tab_a_u)
-                return True
-        raise UniversoNaoEncontrado(self.caminho)
-
-    def deletar_universo(self, d_u_nome:str) ->bool:
-        tab_d_u = self.ler_arquivo()
-        for d_universo in tab_d_u:
-            if d_universo['Nome'] == d_u_nome:
-                tab_d_u.remove(d_universo)
-                self.salvar_arquivo(tab_d_u)
-                return True
-        raise UniversoNaoEncontrado(self.caminho)
-
-def nome_universo_valido():
-    while True:
-        n_v_nome = str(input('Nome: '))
-        if n_v_nome:
-            return n_v_nome
-        else:
-            print('ERRO! Digite um nome para o universo!')
-
-def salvar_universo() ->None:
-    su_nome = nome_universo_valido()
-    su_resumo = str(input('Resumo: '))
-    su_salva = Universo()
-    su_salva.criar(su_nome, su_resumo)
-
-def ler_universo() ->None:
-    lu_nome = nome_universo_valido()
-    lu_uni = Universo()
-    lu_uni.ler(lu_nome)
-    print(f'Nome: {lu_uni.nome}')
-    print(f'Resumo: {lu_uni.resumo}')
-    print(f'Criado: {lu_uni.criado}')
-    print(f'Alterado: {lu_uni.alterado}')
-
-def atualizar_universo() -> None:
-    print('Velho Universo ', end='')
-    vu_nome = nome_universo_valido()
-    au_universo = Universo()
-    au_universo.ler(vu_nome)
-    print('Novo Universo ', end='')
-    au_nome = nome_universo_valido()
-    au_resumo = str(input('Resumo: '))
-    au_universo.atualizar(au_nome, au_resumo)
-
-def deletar_universo() ->None:
-    du_nome = nome_universo_valido()
-    du_universo = Universo()
-    du_universo.ler(du_nome)
-    du_universo.deletar()
